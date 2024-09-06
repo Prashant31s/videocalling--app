a62c13f5781916e7d1c6ca632a1a584a050f3aa3
Prashant shakya <109499944+Prashant31s@users.noreply.github.com>
2024-09-06 16:29:43 +0530

    Update connect.js

--- client/app/components/connect.js
+++ client/app/components/connect.js
@@ -1,3 +1,3 @@
 import { io } from 'socket.io-client';
-const socket =io.connect("https://videocalling-app.onrender.com/")
+const socket =io.connect("https://videocalling-app.onrender.com")
 export default socket
